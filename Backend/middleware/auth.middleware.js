99a624f02ff607e57443283a69cd0106e7ad987d
Amit Kumar Saini <107475338+iamamitkumarsaini@users.noreply.github.com>
2023-06-17 19:54:21 +0530

    Documenting auth.middleware.js

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -2,14 +2,18 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const authentication = (req, res, next) => {
+  //Getting the token through req headers.
   const token = req.headers.authentication?.split(" ")[1];
 
   if (token) {
+    // Verifying if the token is a valid token or not.
     const decoded = jwt.verify(
       token,
       process.env.secret_key,
       (err, decoded) => {
         if (decoded) {
+          // If token is valid and the req url matches this condtion
+        // - then it will add a new key as user_id to make relation with other collections 
           if (req.url === "/add/cart" || req.url === "/get/user/cart") {
             const user_id = decoded._id;
             req.body.user_id = user_id;
@@ -20,11 +24,13 @@ const authentication = (req, res, next) => {
             next();
           }
         } else {
+          // if token is not verified theb sends this response
           res.send({ message: "Please login first" });
         }
       }
     );
   } else {
+    // If no token is send in headers then sends this response
     res.send({ message: "Please login first" });
   }
 };
