f0231dc8eba39d78c472b1984c652bf49765e604
Amit Kumar Saini <107475338+iamamitkumarsaini@users.noreply.github.com>
2023-06-17 20:00:39 +0530

    documenting index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,26 +11,34 @@ const { cartRoute } = require("./Routes/Usercart.route");
 
 const app = express();
 
+// Using cors as middleware for allowing the cross origin access.
 app.use(cors({
     origin:"*"
 }));
 
+// Parsing request body as JSON
 app.use(express.json());
 
-
+// Default route
 app.get("/", (req,res) => {
     
     res.status(200).send({"message": "Welcome to Mobio Solution"});
 })
 
+// User login and signup routes
 app.use("/", userRoutes)
+
+// Products routes for retriving all products with any additional varification
 app.use("/", productsRoute);
 
+// Using middleware for private routes. If no valid token provided then the req cannot be made to the desired route
 app.use(authentication);
+
+// user Cart and order route.
 app.use("/", cartRoute);
 
 
-
+// listening server on a port
 app.listen(process.env.port, async(req,res) => {
 
     try {
